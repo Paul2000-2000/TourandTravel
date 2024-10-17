66b3b6e811252bf573a87be4b1d95c737c9139d3
Paul2000-2000 <alexapaul2000@yahoo.com>
2024-10-17 10:51:21 +0300

    quickliks done, need responsive

--- src/views/quicklinks/quicklinks.js
+++ src/views/quicklinks/quicklinks.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Video from "../../assets/footer.mp4";
 import "./quicklinks.css";
+import Logo from "../../assets/logo.png";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { MdOutlineSmartphone } from "react-icons/md";
+import { FaRegAddressCard } from "react-icons/fa";
+import { TiArrowRightOutline } from "react-icons/ti";
 
 const quicklinks = () => {
   return (
@@ -12,45 +19,95 @@ const quicklinks = () => {
         </video>
       </div>
       <div className="quicklinks-content">
-        <div>
-          <div>
-            <img />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              facere ab hic accusamus omnis dolor voluptatibus illo, tempore eum
-              tenetur.
-            </p>
-            <p>Noida, Uttar Pradesh</p>
-            <p>+123 456 789</p>
+        <div className="quicklinks-content-first">
+          <div className="quicklinks-content-first-div-first">
             <div>
-              <p></p>
-              <p></p>
-              <p></p>
+              <img src={Logo} alt="" id="quicklinks-content-first-div-logo" />
+              <p id="quicklinks-content-first-div-desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                facere ab hic accusamus omnis dolor voluptatibus illo, tempore
+                eum tenetur.
+              </p>
+            </div>
+            <div className="twoel">
+              <FaRegAddressCard />
+              <p id="quicklinks-content-first-div-auth">Noida, Uttar Pradesh</p>
+            </div>
+            <div className="twoel">
+              <MdOutlineSmartphone />
+              <p id="quicklinks-content-first-div-numb">+123 456 789</p>
+            </div>
+            <div className="quicklinks-content-first-div-soc">
+              <FaFacebook style={{ width: "25px", height: "25px" }} />
+              <FaInstagram style={{ width: "25px", height: "25px" }} />
+              <FaLinkedin style={{ width: "25px", height: "25px" }} />
             </div>
           </div>
-          <div>
-            <h2>Important Links</h2>
-            <p>Home</p>
-            <p>About</p>
-            <p>Best Places</p>
-            <p>Blogs</p>
+          <div className="quicklinks-content-first-div ">
+            <h2 className="quicklinks-content-first-div-title">
+              Important Links
+            </h2>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Home</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">About</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Best Places</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Blogs</p>
+            </div>
           </div>
-          <div>
-            <h2>Important Links</h2>
-            <p>Home</p>
-            <p>About</p>
-            <p>Best Places</p>
-            <p>Blogs</p>
+          <div className="quicklinks-content-first-div ">
+            <h2 className="quicklinks-content-first-div-title">
+              Important Links
+            </h2>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Home</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">About</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Best Places</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Blogs</p>
+            </div>
           </div>
-          <div>
-            <h2>Important Links</h2>
-            <p>Home</p>
-            <p>About</p>
-            <p>Best Places</p>
-            <p>Blogs</p>
+          <div className="quicklinks-content-first-div ">
+            <h2 className="quicklinks-content-first-div-title">
+              Important Links
+            </h2>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Home</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">About</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Best Places</p>
+            </div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <TiArrowRightOutline />
+              <p className="quicklinks-content-first-div-item">Blogs</p>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="quicklinks-content-s">
           <p>@copyright 2024 All rights reserved || Made with ❤️ by TCJ</p>
         </div>
       </div>
